72815fe14f54805cb0672cc34f316a57b44bb8a7
inuscript <inuscript@gmail.com>
2016-08-29 00:20:35 +0900

    wip

--- docs/multiple.js
+++ docs/multiple.js
@@ -22,7 +22,7 @@ const elemAnimate = (label, ...props) => {
 
 const aphroditeAnimate = (label, ...props) => {
   let elem = createElement(label)
-  let animatePropsArray = convert(...props)
+  let animatePropsArray = convertMultiple(...props)
   let pr = animatePropsArray.reduce( (prev, curr, index) => {
     let key = `animate-${index}`
     let a = { [key]: curr }
@@ -37,7 +37,7 @@ const aphroditeAnimate = (label, ...props) => {
 
 const freestyleAnimate = (label, ...props) => {
   let Style = FreeStyle.create()
-  let animateProps = convert(...props)[0]
+  let animateProps = convertMultiple(...props)[0]
   let ANIMATION = Style.registerKeyframes(animateProps.animationName)
   let STYL = Style.registerStyle(Object.assign(animateProps, {
     animationName: ANIMATION
@@ -64,86 +64,86 @@ doAnimate("Sample 1", {
   duration: 500,
   iterations: Infinity,
 })
-
-doAnimate("Easing with keyframes", {
-  opacity: [0.5, 1],
-  transform: ['scale(0.5)', 'scale(1)'],
-  easing: 'ease-in-out',
-}, {
-  direction: 'alternate',
-  duration: 500,
-  iterations: Infinity,
-})
-
-doAnimate("keyframes as array", [{
-  opacity: 0.5,
-  transform: 'scale(0.5)',
-}, {
-  opacity: 1,
-  transform: 'scale(1)'
-}], {
-  direction: 'alternate',
-  duration: 500,
-  iterations: Infinity,
-})
-doAnimate("keyframes as multiple frame array", [{
-  opacity: 0.5,
-  transform: 'scale(0.5)',
-}, {
-  opacity: 0.7,
-  transform: 'scale(0.7)',
-}, {
-  opacity: 0.3,
-  transform: 'scale(0.3)',
-}, {
-  opacity: 1,
-  transform: 'scale(1)',
-}], {
-  direction: 'alternate',
-  duration: 500,
-  iterations: Infinity,
-})
-
-doAnimate("keyframes with offset", [{
-  opacity: 1.2,
-  transform: 'scale(1.2)',
-}, {
-  opacity: 0.5,
-  transform: 'scale(0.5)',
-  offset: 0.3
-}, {
-  opacity: 0.7,
-  transform: 'scale(0.7)',
-  offset: 0.5,
-}, {
-  opacity: 1,
-  transform: 'scale(1)',
-  offset: 1,
-}], {
-  direction: 'alternate',
-  duration: 500,
-  iterations: Infinity,
-})
-
-doAnimate("multiple offset no easing", [{
-  background: 'red',
-  transform: 'none',
-}, {
-  offset: 0.1,
-  transform: 'translateY(100px)',
-}, {
-  offset: 0.2,
-  transform: 'translate(100px, 100px)',
-}, {
-  offset: 0.4,
-  transform: 'translateX(100px)',
-}, {
-  background: 'blue',
-  transform: 'none',
-}], {
-  duration: 4000,
-  iterations: Infinity,
-})
+// 
+// doAnimate("Easing with keyframes", {
+//   opacity: [0.5, 1],
+//   transform: ['scale(0.5)', 'scale(1)'],
+//   easing: 'ease-in-out',
+// }, {
+//   direction: 'alternate',
+//   duration: 500,
+//   iterations: Infinity,
+// })
+// 
+// doAnimate("keyframes as array", [{
+//   opacity: 0.5,
+//   transform: 'scale(0.5)',
+// }, {
+//   opacity: 1,
+//   transform: 'scale(1)'
+// }], {
+//   direction: 'alternate',
+//   duration: 500,
+//   iterations: Infinity,
+// })
+// doAnimate("keyframes as multiple frame array", [{
+//   opacity: 0.5,
+//   transform: 'scale(0.5)',
+// }, {
+//   opacity: 0.7,
+//   transform: 'scale(0.7)',
+// }, {
+//   opacity: 0.3,
+//   transform: 'scale(0.3)',
+// }, {
+//   opacity: 1,
+//   transform: 'scale(1)',
+// }], {
+//   direction: 'alternate',
+//   duration: 500,
+//   iterations: Infinity,
+// })
+// 
+// doAnimate("keyframes with offset", [{
+//   opacity: 1.2,
+//   transform: 'scale(1.2)',
+// }, {
+//   opacity: 0.5,
+//   transform: 'scale(0.5)',
+//   offset: 0.3
+// }, {
+//   opacity: 0.7,
+//   transform: 'scale(0.7)',
+//   offset: 0.5,
+// }, {
+//   opacity: 1,
+//   transform: 'scale(1)',
+//   offset: 1,
+// }], {
+//   direction: 'alternate',
+//   duration: 500,
+//   iterations: Infinity,
+// })
+// 
+// doAnimate("multiple offset no easing", [{
+//   background: 'red',
+//   transform: 'none',
+// }, {
+//   offset: 0.1,
+//   transform: 'translateY(100px)',
+// }, {
+//   offset: 0.2,
+//   transform: 'translate(100px, 100px)',
+// }, {
+//   offset: 0.4,
+//   transform: 'translateX(100px)',
+// }, {
+//   background: 'blue',
+//   transform: 'none',
+// }], {
+//   duration: 4000,
+//   iterations: Infinity,
+// })
 
 doAnimate("(X)multiple offset with easing (first)", [{
   background: 'red',
