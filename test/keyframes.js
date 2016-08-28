35f30761eeda06fcfa139d7ddb140a016633a8f8
inuscript <inuscript@gmail.com>
2016-08-28 21:29:47 +0900

    array pattenr

--- test/keyframes.js
+++ test/keyframes.js
@@ -16,12 +16,22 @@ describe('convertKeyframes', function () {
     }]
     assert.deepEqual(convertKeyframes(input), expect)
   })
-  it('convert object with easing', () => {
-    const input = {
-      opacity: [0.5, 1],
-      transform: ['scale(0.5)', 'scale(1)'],
-      easing: 'ease-in-out',
-    }
-    console.log(convertKeyframes(input))
+  // it('convert object with easing', () => {
+  //   const input = {
+  //     opacity: [0.5, 1],
+  //     transform: ['scale(0.5)', 'scale(1)'],
+  //     easing: 'ease-in-out',
+  //   }
+  //   console.log(convertKeyframes(input))
+  // })
+  it('enable array', () => {
+    const input = [{
+      opacity: 0.5,
+      transform: 'scale(0.5)',
+    }, {
+      opacity: 1,
+      transform: 'scale(1)'
+    }]
+    assert.deepEqual(convertKeyframes(input), input)
   })
 });
