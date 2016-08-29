df60067aebe13937710ff1892e78f98b14d5fd52
inuscript <inuscript@gmail.com>
2016-08-29 20:27:38 +0900

    offset property testing

--- test/timings.js
+++ test/timings.js
@@ -2,12 +2,21 @@ const timings = require('../lib/timings')
 const assert = require('assert')
 
 describe('timings', function () {
-  it('Defualt array', () => {
+  it('Should normal array keyframes to regular interval', () => {
     const result = timings([
       {color: 'red'},
       {color: 'green'},
       {color: 'blue'},
     ])
     assert.deepEqual(result, [0, 50, 100])
+  })
+  it('Should use offset if array has property', () => {
+    const result = timings([
+      {color: 'red'},
+      {color: 'yellow', offset: 0.23},
+      {color: 'green', offset: 0.9},
+      {color: 'blue', offset: 1},
+    ])
+    assert.deepEqual(result, [0, 23, 90, 100])
   })
 })
