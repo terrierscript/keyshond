7f7ebdee5ec6bae28f62093c2f0b288ba377e022
inuscript <inuscript@gmail.com>
2016-08-29 10:07:08 +0900

    underscore unused

--- src/timings.js
+++ src/timings.js
@@ -1,5 +1,5 @@
 
-const timings = (keyframes, options) => {
+const timings = (keyframes, _options) => {
   return keyframes.map( (frame, index) => {
     if(index === 0) return 0
     if(frame.offset !== undefined && frame.offset <= 1 && frame.offset >= 0){
