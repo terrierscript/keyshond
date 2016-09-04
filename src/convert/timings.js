9e753f3814821cafc6bfafe8f9a5cc2794a5f42a
inuscript <inuscript@gmail.com>
2016-09-04 23:16:28 +0900

    feat(timings): sanitize timing bound object offset

--- src/convert/timings.js
+++ src/convert/timings.js
@@ -1,12 +1,12 @@
 const zipObject = (keys, values) => {
   return keys.reduce((prev, key, index) => {
     return Object.assign({}, prev, {
-      [key]: values[index]
+      [key]: cleanObject(values[index])
     })
   }, {})
 }
 
-const calcTiming = (keyframes) => {
+const calcOffset = (keyframes) => {
   return keyframes.map((frame, index) => {
     if (index === 0) return 0
     if (frame.offset !== undefined && frame.offset <= 1 && frame.offset >= 0) {
@@ -16,8 +16,13 @@ const calcTiming = (keyframes) => {
   })
 }
 
+const cleanObject = (value) => {
+  delete value.offset
+  return value
+}
+
 const calcPrecentageTimings = (keyframes) => {
-  return calcTiming(keyframes)
+  return calcOffset(keyframes)
     .map(offset => offset * 100)
     .map(precentage => `${precentage}%`)
 }
