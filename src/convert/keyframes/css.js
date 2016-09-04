fd894819a28b307374f2974bf5cf70b6bb8d7bcf
inuscript <inuscript@gmail.com>
2016-09-04 23:23:02 +0900

    style(lint): fix lint

--- src/convert/keyframes/css.js
+++ src/convert/keyframes/css.js
@@ -2,7 +2,7 @@
 
 // see: https://github.com/jedmao/parse-css-dimension/blob/master/index.js#L25-L30
 const tryParsePercentage = (value) => {
-  if(typeof value === "number"){
+  if (typeof value === 'number') {
     return value
   }
   if (!/%$/.test(value)) {
