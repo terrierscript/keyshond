ffa971b7fc2c851670c6beb58b53a3629505fd53
inuscript <inuscript@gmail.com>
2016-09-07 23:55:27 +0900

    style(space): append space

--- src/animate.js
+++ src/animate.js
@@ -5,7 +5,7 @@ const defaultOptions = {
 
 const animate = (keyframeInput, animateEffectOrDuration, opt = {}) => {
   const options = Object.assign({}, defaultOptions, opt)
-  const {keyframs, animateEffect} = convert(keyframeInput, animateEffectOrDuration)
+  const { keyframs, animateEffect } = convert(keyframeInput, animateEffectOrDuration)
   const animationName = options.generateAnimationName(keyframs)
   const animationNameOption = animationName ? { animationName: animationName } : {}
   return Object.assign({},
